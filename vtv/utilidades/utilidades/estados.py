90598794cc52ebc63a192b01803281d7217ae742
Luis Florez <lefway@gmail.com>
2012-06-05 17:36:14 -0430

    modulo de estados

---
+++ vtv/utilidades/utilidades/estados.py
@@ -0,0 +1,7 @@
+# -*- coding:  utf-8 -*-
+"""
+   Este modulo define el vocabulario genericos de la aplicacion
+   
+   ESTADOS_VZLA, contiene la lista de estados de venezuela
+"""
+ESTADOS_VZLA=['Zulia','Yaracuy','Vargas','Trujillo','Tachira','Sucre','Portugesa','Monagas','Miranda','Merida']
